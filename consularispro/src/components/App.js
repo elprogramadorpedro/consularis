b545d709da6c678845f06497eb9006d0c9cea09d
pedro imbrech <elprogramadorpedro@gmail.com>
2022-11-28 11:47:12 -0300

    init consularis

---
+++ consularispro/src/components/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Consularis</h1>
+      <p>Resguardar el derecho civil a nivel nacional e internacional brindando confianza y seguridad a todas las personas que confian en nosotros.</p>
+    </div>
+  );
+}
+
+export default App;
